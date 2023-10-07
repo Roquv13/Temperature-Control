03281e7bfb7d12fbfbd01a24680e9ef8bf06bcbf
Cezary Olszewski <czarekolszewski99@gmail.com>
2023-10-07 19:43:47 +0200

    Add display heat up plan

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,18 +1,19 @@
 import java.util.Scanner;
 
 public class UserInterface {
+    Scanner scanner = new Scanner(System.in);
+
     boolean isLocked = false;
     double temperature = 22;
     char celsius = '\u2103';
     char fahrenheit = '\u2109';
     public void displayUserSelection() {
-        Scanner scanner = new Scanner(System.in);
         boolean select = true;
 
         do {
             System.out.println("1. Display actual temperature");
             System.out.println("2. Temperature control");
-            displayLockInterface();
+            displayLockInterface(); //Lock or unlock
             System.out.println("4. Heat up plans");
             System.out.println("5. Settings");
             System.out.println("6. Exit");
@@ -23,16 +24,34 @@ public class UserInterface {
 
             switch (userSelection) {
                 case 1 -> System.out.println("Actual temperature: " + temperature + celsius);
-                case 2 -> {
-                    System.out.println("case2");
-                }
+                case 2 -> displayTempControlSelection();
                 case 3 -> {
                     if (isLocked == false) isLocked = true;
                     else if (isLocked == true) isLocked = false;
                 }
-                case 4 -> System.out.println("case4");
+                case 4 -> displayHeatUpPlans();
                 case 5 -> System.out.println("case5");
                 case 6 -> select = false;
+            }
+        } while (select);
+    }
+    public void displayTempControlSelection() {
+        boolean select = true;
+
+        System.out.println("Type of temperature control:");
+        System.out.println("1. Automatic");
+        System.out.println("2. Manual");
+        System.out.println("3. Back");
+        System.out.println();
+
+        System.out.println("Select:");
+        int userSelection = scanner.nextInt();
+
+        do {
+            switch (userSelection) {
+                case 1 -> System.out.println("Automatic");
+                case 2 -> System.out.println("Manual");
+                case 3 -> select = false;
             }
         } while (select);
     }
@@ -44,21 +63,28 @@ public class UserInterface {
             System.out.println("3. Unlock interface");
         }
     }
-    public void displayTempControlSelection() {
-        System.out.println("Type of temperature control:");
-        System.out.println("1. Automatic");
-        System.out.println("2. Manual");
-        System.out.println("3. Back");
-        System.out.println();
-    }
 
     public void displayHeatUpPlans() {
+        boolean select = true;
+        
         System.out.println("Heat up plans:");
         System.out.println("1. Whole week");
         System.out.println("2. Working days + weekend");
         System.out.println("3. Working days + sunday");
         System.out.println("4. Back");
         System.out.println();
+
+        System.out.println("Select:");
+        int userSelection = scanner.nextInt();
+
+        do {
+            switch (userSelection) {
+                case 1 -> System.out.println("Whole week");
+                case 2 -> System.out.println("Working days + weekend");
+                case 3 -> System.out.println("Working days + sunday");
+                case 4 -> select = false;
+            }
+        } while (select);
     }
 
 }
