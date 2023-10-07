f791c0eb79062049dcbf54e4f333d12036e69e03
Cezary Olszewski <czarekolszewski99@gmail.com>
2023-10-07 15:44:20 +0200

    Add UserInteraface class and userSelection method

---
+++ src/UserInterface.java
@@ -0,0 +1,10 @@
+public class UserInterface {
+    public void userSelection() {
+        System.out.println("1. Display actual temperature");
+        System.out.println("2. Temperature control");
+        System.out.println("3. Lock interface");
+        System.out.println("5. Heat up plan");
+        System.out.println("6. Settings");
+        System.out.println("7. Exit");
+    }
+}
