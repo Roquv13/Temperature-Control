b738da24c0fab66d5abf61bcc1d87ddb7f63b8c6
Roquv13 <czarekolszewski99@gmail.com>
2023-10-09 12:34:50 +0200

    Main.java delete not used class and Temperature_Manual.java delete not used method

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,5 @@ public class Main {
         User_Interface userInterface = new User_Interface();
 
         userInterface.displayUserSelection();
-
-        //userInterface.displayTempControlSelection();
-
-        //userInterface.displayHeatUpPlans();
     }
 }
--- src/Temperature_Manual.java
+++ src/Temperature_Manual.java
@@ -1,10 +1,6 @@
 public class Temperature_Manual {
     private double temperature;
 
-//    public Temperature_Manual() {
-//        this.temperature = initialTemperature;
-//    }
-
     public double getTemperature() {
         return this.temperature;
     }
